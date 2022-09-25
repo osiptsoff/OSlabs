8444094d3f398105527c3dcb29a6f1e18c4e7a0d
Никита Осипцов <osipczovnikita@gmail.com>
2022-09-26 00:49:36 +0300

    implemented folders processing

--- lab1/folderhandler.h
+++ lab1/folderhandler.h
@@ -5,12 +5,57 @@
 #include <windows.h>
 #include <fileapi.h>
 
-// ADD documentation
+using std::cout;
+using std::wcin;
+
+/*
+	makeFolder() creates a directory with path received from user if no such folder exists;
+	path to folder should be full: D:\Users\Me\Folder for example,
+	in this case in D:\Users\Me\ a new folder named Folder will be created;
+	otherwise new folder can be created only in the same location with this program;
+	this function does not work recursively (i.e. group of nested folders cannot be created by one call)
+*/
 void makeFolder() {
-	std::cout << "\nPlaceholder for makeFolder()\n";
+	wchar_t answer;
+	wchar_t path[255];
+
+	cout << "\nDo you really want to create new directory? 'y' for 'yes': ";
+	wcin >> answer;
+	wcin.ignore(INT_MAX, '\n');
+
+	if (towupper(answer) == 'Y') {
+		cout << "Enter the full name of folder, max 255 symbols (D:\\Users\\Me\\NewFolder for example): ";
+		wcin.getline(path, 255, L'\n');
+
+		if (CreateDirectory(path, NULL)) 
+			cout << "Directory succsessfully created.\n";
+		else
+			cout << "Error happened. Probably you entered incorrect name.\n";
+	}
 }
 
-// ADD documentation
+/*
+	removeFolder() removes a directory with path received from user;
+	path to folder should be full: D:\Users\Me\Folder for example,
+	in this case in D:\Users\Me\ a folder named Folder will be deleted;
+	otherwise only directory located in the same place with this program will be removed;
+	this function can remove only empty folders
+*/
 void removeFolder() {
-	std::cout << "\nPlaceholder for removeFolder()\n";
+	wchar_t answer;
+	wchar_t path[255];
+
+	cout << "\nDo you really want to delete a directory? 'y' for 'yes': ";
+	wcin >> answer;
+	wcin.ignore(INT_MAX, '\n');
+
+	if (towupper(answer) == 'Y') {
+		cout << "Enter the full name of folder, max 255 symbols (D:\\Users\\Me\\NewFolder for example): ";
+		wcin.getline(path, 255, L'\n');
+
+		if (RemoveDirectory(path))
+			cout << "Directory succsessfully removed.\n";
+		else
+			cout << "Error happened. Probably you entered incorrect name or a folder was not empty.\n";
+	}
 }
