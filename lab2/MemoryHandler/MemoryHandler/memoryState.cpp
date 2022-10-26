bee4aa3c8b1b07fee39328ccf2bf167e18405f98
Никита Осипцов <osipczovnikita@gmail.com>
2022-10-26 21:54:44 +0300

    implemented memory allocation and memory state getting

--- lab2/MemoryHandler/MemoryHandler/memoryState.cpp
+++ lab2/MemoryHandler/MemoryHandler/memoryState.cpp
@@ -1,9 +1,85 @@
 #include <iostream>
+#include <windows.h>
+#include <fileapi.h>
 
 #include "memoryhandler.h"
 
 using std::cout;
+using std::wcin;
 
 void memoryState() {
-	cout << "Placeholder for memoryState()";
+	PMEMORY_BASIC_INFORMATION memInfo;
+	PVOID address = nullptr;
+	wchar_t answer;
+
+	// confirming action
+	cout << "Do you really want to get information about specific region of memory? ('y' for 'yes'): ";
+	wcin >> answer;
+	wcin.ignore(INT_MAX, '\n');
+	if (towupper(answer) != 'Y')
+		return;
+
+	// specify region
+	cout << "Enter the address belonging to region you want to retrieve info about (hex): ";
+	wcin >> std::hex >> address >> std::dec;
+	wcin.ignore(INT_MAX, '\n');
+	if (address == nullptr) {
+		cout << "Failed to parse address.\n";
+		return;
+	}
+
+	// get required info
+	memInfo = new MEMORY_BASIC_INFORMATION();
+	VirtualQuery(address, memInfo, sizeof(*memInfo));
+
+	// check for mistakes
+	switch (DWORD err = GetLastError()) {
+	case 0:
+		break;
+	case ERROR_INVALID_PARAMETER:
+		cout << "Failed to get info. Region is not available for this process (perhaps it was not allocated).\n";
+		SetLastError(0);
+		return;
+	default:
+		cout << "Failed to get info. Unpredicted mistake. Error " << err << ".\n";
+		SetLastError(0);
+		return;
+	}
+
+	// print info
+	cout << "Base address of desired region: " << memInfo->BaseAddress << ";\n";
+	cout << "Base address of all memory allocated by this process: " << memInfo->AllocationBase << ";\n";
+	cout << "Word of region's protection option (hex): " << std::hex << memInfo->AllocationProtect << std::endl
+		<< "\t(more details: https://learn.microsoft.com/en-us/windows/win32/memory/memory-protection-constants)"
+		<< ";\n";
+	cout << "Size of the region (dec): " << std::dec << memInfo->RegionSize << ";" << std::endl;
+	switch (memInfo->State) {
+		case MEM_COMMIT:
+			cout << "Physical storage was allocated for this region;\n";
+			break;
+		case MEM_FREE:
+			cout << "This memory was not reserved neither has not been target of commit;\n";
+			break;
+		case MEM_RESERVE:
+			cout << "Region is reserved with no physical storage;\n";
+			break;
+		default:
+			cout << "Memory state: ERROR;\n";
+	}
+	switch (memInfo->Type) {
+		case MEM_IMAGE:
+			cout << "Memory pages within the region are mapped into the view of an image section;\n";
+			break;
+		case MEM_MAPPED:
+			cout << "Memory pages within the region are mapped into the view of a section;\n";
+			break;
+		case MEM_PRIVATE:
+			cout << "Memory pages within the region are private (that is, not shared by other processes);\n";
+			break;
+		default:
+			cout << "Memory type: ERROR;\n";
+	}
+
+	// free used memory
+	delete memInfo;
 }
--- lab2/MemoryHandler/MemoryHandler/reserveRegion.cpp
+++ lab2/MemoryHandler/MemoryHandler/reserveRegion.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
+#include <windows.h>
+#include <fileapi.h>
 
 #include "memoryhandler.h"
 
 using std::cout;
+using std::wcin;
 
 void reserveRegion() {
-	cout << "Placeholder for reserveRegion()";
+	PVOID address = nullptr;
+	SIZE_T regsize = 4096;
+	wchar_t answer;
+
+	// confirming action
+	cout << "Do you really want to reserve a memory region? ('y' for 'yes'): ";
+	wcin >> answer;
+	wcin.ignore(INT_MAX, '\n');
+	if (towupper(answer) != 'Y')
+		return;
+
+	// get desired address if required
+	cout << "Do you want to specify an address to reserve? ('y' for 'yes'): ";
+	wcin >> answer;
+	wcin.ignore(INT_MAX, '\n');
+	if (towupper(answer) == 'Y') {
+		cout << "Enter the start of region you want to reserve (hex): ";
+		wcin >> std::hex >> address >> std::dec;
+		wcin.ignore(INT_MAX, '\n');
+
+		cout << "Enter the size of region you want to reserve (dec): ";
+		wcin >> regsize;
+		wcin.ignore(INT_MAX, '\n');
+	}
+	else
+		cout << "Address will be chosen automatically."
+		<< "\nSize of region will be 4096 bytes.\n";
+
+	// reserve a region
+	address = VirtualAlloc(address, regsize, MEM_RESERVE, PAGE_READWRITE);
+
+	// check if allocation was succsessful
+	if (address)
+		cout << "Allocation was succseffsul.\n"
+			<< "\tStarting address is " << std::dec << address << ".\n";
+	else {
+		cout << "Allocation failed. Error " << GetLastError() << ".\n";
+		SetLastError(0);
+	}
 }
