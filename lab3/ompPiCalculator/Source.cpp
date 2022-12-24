129664ccce5c577298f7f8db7accdfb5480bd8ab
Никита Осипцов <osipczovnikita@gmail.com>
2022-12-24 22:15:20 +0300

    changed omp lab 3 part to use dynamic scheduling

--- lab3/ompPiCalculator/Source.cpp
+++ lab3/ompPiCalculator/Source.cpp
@@ -6,7 +6,7 @@
 
 const unsigned requiredPrecision = 100000000;
 const unsigned operationsPerBlock = 100;
-const unsigned threadsNumber = 12;
+const unsigned threadsNumber = 16;
 
 int main() {
 	double pi = 0;
@@ -19,23 +19,10 @@ int main() {
 
 	startTime = GetTickCount();
 
-	// outer for-loop will be performed by threadsNumber threads,
-	// so they will execute calculations by blocks with operationsPerBlock operations in each one;
-	// every thread has increment variable where result of its calculations is accumulated,
-	// this variable then will be added to total result (pi variable) in synchronized section
-	#pragma omp parallel
-	{
-		double increment = 0;
+	#pragma omp parallel for schedule(dynamic, operationsPerBlock) reduction(+:pi)
+		for (int i = 0; i < requiredPrecision; ++i)
+			pi += 4 / (1 + ((i + 0.5) / requiredPrecision) * ((i + 0.5) / requiredPrecision));
 
-		#pragma omp for
-		for (int block = 0; block < blocksNumber; ++block)
-			for (int i = block * operationsPerBlock; i < block * operationsPerBlock + operationsPerBlock; ++i)
-				increment += 4 / (1 + ((i + 0.5) / requiredPrecision) * ((i + 0.5) / requiredPrecision));
-
-		omp_set_lock(&lock);
-		pi += increment;
-		omp_unset_lock(&lock);
-	}
 	endTime = GetTickCount();
 
 	pi /= requiredPrecision;
